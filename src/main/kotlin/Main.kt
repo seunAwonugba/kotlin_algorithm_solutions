99d34519aa4ad868e449d940ab2e613f0b8a3810
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-11-18 21:13:05 +0100

    implement min max sum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 fun main() {
     val arrayList = listOf(
-        5, 1, -5, 0, 2, 10, -4, 15, 30, 11, 35, 11, -54, 22, 10, 0 -50, 21, -15, 12, 20, -14, 25, 85, 71, 0, -35, 12, 100, 31, 13
+        5, 1, -5, 0, 2, 10, -4, 15, 30, 11, 35, 11, -54, 22, 10, 0, -50, 21, -15, 12, 20, -14, 25, 85, 71, 0, -35, 12, 100, 31, 13
     )
     //find a value from a list
+
 
     //linear search time complexity of O(n)
     fun checkNumberExist(num : Int, arr : List<Int>) : Boolean{
@@ -74,8 +75,6 @@ fun main() {
     println(optimisedCheckNumberExist( num = 111, arr = arrayList))
 
 
-
-
     fun sumOfANumberFromRange(n : Int) : Int{
         var result = 0
         for (i in 1..n){
@@ -88,6 +87,7 @@ fun main() {
 
     //hackerrank algorithm
     //plus minus
+    //Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero
     fun plusMinus(arr : List<Int>){
         val arrSize = (arr.size).toFloat()
         var positive = 0f
@@ -114,4 +114,67 @@ fun main() {
     }
 
     plusMinus(arrayList)
+
+    var newArray = listOf<Int>(3,2,3)
+
+    fun twoSums(arr : List<Int>, target: Int) : IntArray{
+        val twoArray = intArrayOf()
+
+        for (i in 0.. arr.size-1){
+            var reverseValue = target - arr[i]
+
+            for (e in 0..arr.size-1){
+                if (reverseValue == arr[e]){
+                    return twoArray.plus(i).plus(e)
+
+                }
+            }
+
+
+        }
+
+        return twoArray
+    }
+
+    println(twoSums(newArray, 9))
+
+    fun staircase(n : Int){
+        for (i in 1..n){
+            val requiredSpace = n - i
+
+            for (j in 1 .. requiredSpace){
+                print(" ")
+            }
+
+            for (j in 1 .. i){
+                print("#")
+            }
+            println()
+        }
+    }
+
+    staircase(3)
+
+    fun minMaxSum(arr : Array<Int>){
+        var sortedArray = arr.sorted()
+
+        var totalSum = 0
+        for (i in sortedArray){
+            totalSum += i
+        }
+
+        val maxVal = totalSum - sortedArray.first()
+        val minVal = totalSum - sortedArray.last()
+
+        println("$minVal $maxVal" )
+    }
+
+    minMaxSum(arr = arrayOf(1,3,5,7,9))
 }
+
+//    #
+//   ##
+//  ###
+// ####
+
+
